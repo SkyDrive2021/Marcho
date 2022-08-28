795f7bab2b8fce605d4e96fe867afb9d0f148a11
SkyDrive2021 <vladbezbozhny@gmail.com>
2022-08-28 13:17:49 +0300

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$(function(){
+$(function () {
 
+  $('.top-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 1500
+  });
 });
+
+
+
+
+
+
+
