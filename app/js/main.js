7bcd1f2560786ff0fa3b3df951eb7e31460bf7b5
Vla0.d <vladbezbozhny@gmail.com>
2023-02-04 22:25:48 +0200

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,45 @@
 $(function () {
 
-  $('.shop-content__filter-btn').on('click', function (){
-    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
-    $(this).addClass ('shop-content__filter-btn--active');
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
   });
 
-  $('.button-list').on('click', function(){
-    $('.product-item').addClass ('product-item--list');
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
   });
 
   $('.button-grid').on('click', function () {
-    $('.product-item').removeClass ('product-item--list');
+    $('.product-item').removeClass('product-item--list');
   });
 
-  $('.select-style').styler();
+  $('.select-style, .product-one__item-num').styler();
 
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
-    onStart: function (data){
+    onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
-    onChange: function (data){
+    onChange: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
 
